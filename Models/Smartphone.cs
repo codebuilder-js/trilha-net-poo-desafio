e601a3ca65f18684da6a19a488dddfa234d2847d
Rafael Alves <codebuilder-js@gmail.com>
2024-12-02 13:09:30 -0300

    Adicionado funcionalidade das classes Iphone.cs Nokia.cs Smartphone.cs e testes no Program.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,16 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string Imei { get; set; }
+        pritave int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = Modelo;
+            Imei = Imei;
+            Memoria = Memoria;
         }
 
         public void Ligar()
